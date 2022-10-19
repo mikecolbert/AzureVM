334a6fd25838a7a19c49a7e08e5146bfd2779f16
Mike Colbert <michael-colbert@uiowa.edu>
2022-10-18 20:16:15 -0500

    git clone

--- configure-nginx2.sh
+++ configure-nginx2.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 
 # Install Nginx.
 sudo apt-get install -y nginx
+sudo apt install -y git-all 
 
-# Get the home page.
-curl https://raw.githubusercontent.com/mikecolbert/AzureVM/main/index.html | sudo tee -a /var/www/html/index.html
+# clone the repository.
+#curl https://raw.githubusercontent.com/mikecolbert/AzureVM/main/index.html | sudo tee -a /var/www/html/index.html
+cd /var/www/html
+git clone https://github.com/mikecolbert/my_portfolio.git
+mv /my_portfolio/* ../*
