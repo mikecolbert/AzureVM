06dfd63ce7dfc05864e25eb8973457518982239b
Mike Colbert <michael-colbert@uiowa.edu>
2022-10-18 21:42:06 -0500

    Update configure-nginx2.sh

--- configure-nginx2.sh
+++ configure-nginx2.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # Update apt cache.
-sudo apt-get update
+#sudo apt-get update
 
 # Install Nginx.
 ECHO "Installing Nginx"
-sudo apt-get install -y nginx
+#sudo apt-get install -y nginx
 ECHO "Installing Git"
-sudo apt install -y git-all 
+#sudo apt install -y git-all 
 
 # clone the repository.
 #curl https://raw.githubusercontent.com/mikecolbert/AzureVM/main/index.html | sudo tee -a /var/www/html/index.html
-cd /var/www/html
+
 rm -R /var/www/html/*
-ECHO "Cloning portfolio"
+
+cd /var/www/html
+
 git clone https://github.com/mikecolbert/my_portfolio.git
-ECHO "Copying files into web directory"
-cp -R my_portfolio/* /var/www/html/*
-ECHO "Deleting .github folder"
-rm -R /var/www/html/.github
+
+cp -r my_portfolio/* /var/www/html/*
+
+#rm -R /var/www/html/.github
 
 
