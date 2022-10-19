ac384e71bc728c2cdd9b374119121d58a155edfa
Mike Colbert <michael-colbert@uiowa.edu>
2022-10-18 20:07:47 -0500

    download web page using curl

--- configure-nginx2.sh
+++ configure-nginx2.sh
@@ -6,5 +6,5 @@ sudo apt-get update
 # Install Nginx.
 sudo apt-get install -y nginx
 
-# Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+# Get the home page.
+curl https://raw.githubusercontent.com/mikecolbert/AzureVM/main/index.html | sudo tee -a /var/www/html/index.html
