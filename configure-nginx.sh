3bc95fa4dd34865057c37c377cb36447851f3c72
Mike Colbert <michael-colbert@uiowa.edu>
2022-10-21 09:03:50 -0500

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Update apt cache.
-sudo apt-get update
+# update apt cache and Linux OS
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 
-# Install Nginx.
+# install Nginx and Git
 sudo apt-get install -y nginx
+sudo apt install -y git-all 
 
-# Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+# remove any existing files or folders in the root directory 
+sudo rm -rf /var/www/html/
+
+# remove the existing cloned website directory and any files or subfolders
+sudo rm -rf /var/www/HW6_website
+
+# change into the /var/www/ directory so the repository is cloned into a 
+# folder that is not the root web directory
+cd /var/www/
+
+# clone the repository - it is now in /var/www/HW6_website
+git clone https://github.com/mikecolbert/HW6_website.git
+
+# copy recursively all files and folders into the root html directory for Nginx
+sudo cp -r /var/www/HW6_website/ /var/www/html
+
+# remove the .github folder and all subfolders and files from the root web directory
+sudo rm -rf /var/www/html/.github/
